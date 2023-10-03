9050f3c5562639806642c6197c90a4735cb9bd4a
joe-bor <kimborlongan_08@yahoo.com>
2023-10-03 09:25:43 -0700

    added notes regarding ISR & generateStaticParams()

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -18,10 +18,23 @@ export async function generateStaticParams() {
 
   // returns the params we need to render in advance
   // { slug: post.slug }
+
   return posts.map( (post) => ({
     slug: post.slug
   }))
 }
+
+/*
+- client requests for this page `url/blog/${dynamic-route}`
+- generateStaticParams() is called, returns an array of objects
+- returned value is used to create pre-rendered pages, cached in the server for quicker retrievals
+- then every time client requests for this page, Next first checks if it is available in the cache
+   
+    - if it is, it serves the pre-rendered page (while valid)
+      - invalidated after `export const revalidate = 500`
+    
+    - if not valid,  fetch is used to get desired data
+*/
 
 async function BlogPostPage( { params } : Props) {
 
