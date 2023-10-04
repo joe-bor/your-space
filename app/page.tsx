ab0feb993328b44510091d5b108c8909d1405e0c
joe-bor <kimborlongan_08@yahoo.com>
2023-10-03 20:48:14 -0700

    Added new api route to get all users

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
+import { authOptions } from "./api/auth/[...nextauth]/route"
 
 
 
 export default async function Home() {
 
-  const  session  = await getServerSession()
+  const  session  = await getServerSession(authOptions)
   if (!session) {
     redirect('/api/auth/signin')
 
