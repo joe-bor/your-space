5b6d87be69ef4c0598629c738ba0526542be6780
joe-bor <kimborlongan_08@yahoo.com>
2023-10-08 20:01:45 -0700

    add following system schema & api route

---
+++ app/api/follow/route.ts
@@ -0,0 +1,55 @@
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextResponse, NextRequest } from "next/server";
+
+
+export async function POST(req: Request){
+    // grab current logged in user
+    const session = await getServerSession(authOptions)
+    const currentUserEmail = session?.user?.email!
+    // extract user id to be followed ( sent via request object )
+    const { targetId } = await req.json() 
+
+    // get current user id by querying db
+    const currentUserId = await prisma.user
+        .findUnique({where: { email: currentUserEmail }})
+        .then( user => user?.id!)
+
+    // create a new data in Follows model using the info above
+    const record = await prisma.follows.create({
+        data: {
+            followerId: currentUserId,
+            followingId: targetId
+        }
+    })
+
+    return NextResponse.json(record)
+}
+
+
+export async function DELETE(req: NextRequest) {
+    const session = await getServerSession(authOptions)
+    const currentUserEmail = session?.user?.email!
+    const currentUserId = await prisma.user
+     .findUnique({ where: { email: currentUserEmail }})
+     .then( user => user?.id!)
+
+    // https://nextjs.org/docs/app/api-reference/functions/next-request#nexturl
+    const targetUserId = req.nextUrl.searchParams.get("targetUserId")
+
+    // delete record in the db using prisma
+    const record = await prisma.follows.delete({
+        where: {
+            followerId_followingId: {
+                followerId: currentUserId,
+                followingId: targetUserId!
+            }
+        }
+    })
+
+    return NextResponse.json(record)
+
+
+
+}
